ac2dd3ace05c0793ef8bbfdfb9f142e46e4798a1
sven <sven@so36.net>
2013-10-06 16:38:03 +0200

    Start with package list

---
+++ manifests/packages.pp
@@ -0,0 +1,93 @@
+# Packages we need
+
+class packages {
+
+    # Basics
+    package {"build-essential":
+        ensure => present,
+    }
+
+    # Basics
+    package {"htop":
+        ensure => present",
+    }
+
+    # Time sync
+    package {"ntp":
+        ensure => present,
+    }
+
+    # Keep /tmp clean
+    package {"tmpreaper":
+        ensure => present,
+    }
+
+    # for add-apt-repository
+    package {"python-software-properties":
+        ensure => present,
+    }
+
+    # VCS
+    package {"git-core":
+        ensure => present,
+    }
+
+    # VCS
+    package {"subversion":
+        ensure => present,
+    }
+
+    # VCS
+    package {"libxml2-utils":
+        ensure => present,
+    }
+
+    # Word support
+    package {"wv":
+        ensure => present,
+    }
+
+    # PDF support
+    package {"poppler-utils":
+        ensure => present,
+    }
+
+    # bz2 support to extract pypi packages
+    package {"libbz2-dev":
+        ensure => present,
+    }
+
+    # Code analysis
+    package {"ohcount":
+        ensure => present,
+    }
+
+    # Code analysis
+    package {"sloccount":
+        ensure => present,
+    }
+
+    #todo add nodejs and npm packages
+
+    # Robot Framework (This dependency is only necessary when
+    # 'Library  Dialogs' are added to the robot setup, which shouldn't happen)
+    package {"python-tk:
+        ensure => present,
+    }
+
+    # Java is needed to connect the Jenkins slaves
+    package {"openjdk-7-jre":
+        ensure => present,
+    }
+
+    # LXML
+    package {"libxslt1-dev":
+        ensure => present,
+    }
+
+    # LXML
+    package {"libxml2-dev":
+        ensure => present,
+    }
+
+include packages
